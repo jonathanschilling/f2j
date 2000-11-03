8adf58622360799c16c23d70f2ceb395da05570f
seymour <>
2000-11-03 01:49:25 +0000

    adding header file for memory routines.
    kgs

---
+++ f2j/src/f2jmem.h
@@ -0,0 +1,15 @@
+#ifndef F2JMEM_H
+#define F2JMEM_H
+
+#include"f2j.h"
+
+void
+  alloc_error(size_t),
+  f2jfree(void *, size_t),
+  free_fieldref(METHODREF *),
+  free_var_info(struct var_info *),
+  * f2jalloc(size_t),
+  * f2jcalloc(size_t, size_t),
+  * f2jrealloc(void *, size_t);
+
+#endif

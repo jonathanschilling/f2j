5743dea2eb6b0a42262e0cb0d68e36b2336296d6
keithseymour <>
2006-12-13 19:57:07 +0000

    -adding single precision versions of the matrix conversion methods keith

--- f2j/jlapack/src/util/org/netlib/util/MatConv.java
+++ f2j/jlapack/src/util/org/netlib/util/MatConv.java
@@ -37,6 +37,40 @@ public class MatConv
     return mat;
   }
 
+  /*  Method to convert 2D matrices into 1D arrays for
+   *  passing to blas and lapack subroutines.
+   */
+
+  public static float[] floatTwoDtoOneD (float[][]m)
+  {
+    /* We make the assumption here that the matrices are
+     * square (or rectangular), to get the value of
+     * the second index.
+     */
+
+    int ld = m.length;
+    float[] apimatrix = new float[ld * m[0].length];
+
+    for (int i = 0; i < ld; i++)
+      for (int j = 0; j < m[0].length; j++)
+        apimatrix[i + j * ld] = m[i][j];
+
+    return apimatrix;
+  }                             //  Close TwoDtoOneD().
+
+  public static float[][] floatOneDtoTwoD(float [] vec, int ld)
+  {
+    int i,j;
+    float [][] mat = new float [ld][vec.length / ld];
+
+   
+    for (i = 0; i < ld; i++)
+      for (j = 0; j < mat[0].length; j++)
+        mat[i][j] = vec[i + j * ld];
+
+    return mat;
+  }
+
   public static int[] intTwoDtoOneD (int[][]m)
   {
     /* We make the assumption here that the matrices are
@@ -68,6 +102,16 @@ public class MatConv
   }
 
   public static void copyOneDintoTwoD(double [][]mat, double[]vec)
+  {
+    int i,j;
+    int ld = mat.length;
+
+    for (i = 0; i < ld; i++)
+      for (j = 0; j < mat[0].length; j++)
+        mat[i][j] = vec[i + j * ld];
+  }
+
+  public static void copyOneDintoTwoD(float [][]mat, float[]vec)
   {
     int i,j;
     int ld = mat.length;

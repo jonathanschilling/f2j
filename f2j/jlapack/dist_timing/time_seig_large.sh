72762f21a0206be913396754c950c691d04e046a
keithseymour <>
2007-01-30 05:24:29 +0000

    -fixing single precision timing scripts keith

--- f2j/jlapack/dist_timing/time_seig_large.sh
+++ f2j/jlapack/dist_timing/time_seig_large.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.lin.Stimee < SGEPTIM.in
-java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.lin.Stimee < SNEPTIM.in
-java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.lin.Stimee < SSEPTIM.in
-java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.lin.Stimee < SSVDTIM.in
+java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.eig.Stimee < SGEPTIM.in
+java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.eig.Stimee < SNEPTIM.in
+java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.eig.Stimee < SSEPTIM.in
+java -Xmx500M -classpath seigtime.jar:smatgen.jar:../f2jutil.jar:../sblas.jar:../slapack.jar org.netlib.lapack.timing.eig.Stimee < SSVDTIM.in
--- f2j/jlapack/dist_timing/time_seig_small.bat
+++ f2j/jlapack/dist_timing/time_seig_small.bat
@@ -1,4 +1,4 @@
-java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.lin.Stimee < sgeptim.in
-java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.lin.Stimee < sneptim.in
-java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.lin.Stimee < sseptim.in
-java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.lin.Stimee < ssvdtim.in
+java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.eig.Stimee < sgeptim.in
+java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.eig.Stimee < sneptim.in
+java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.eig.Stimee < sseptim.in
+java -Xmx500M -classpath seigtime.jar;smatgen.jar;..\f2jutil.jar;..\sblas.jar;..\slapack.jar org.netlib.lapack.timing.eig.Stimee < ssvdtim.in
